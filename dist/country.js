4f0c5aa07676b7eff067cc186b2360072a96a411
iamchzy <74089229+iamchzy@users.noreply.github.com>
2023-11-13 09:55:52 +0800

    sign-up page

---
+++ dist/country.js
@@ -0,0 +1,265 @@
+var countrySateCityinfo = {
+  "Australia": {
+      "New South Wales": {
+          "Newcastle": ["2300", "2301", "2302"],
+          "Sydney": ["2000", "2001", "2002"]
+      },
+      "Queensland": {
+          "Brisbane": ["4000", "4001", "4002"],
+          "Gold Coast": ["4217", "4218", "4219"]
+      },
+      "South Australia": {
+          "Adelaide": ["5000", "5001", "5002"],
+          "Mount Gambier": ["5290", "5291", "5292"]
+      },
+      "Tasmania": {
+          "Burnie": ["7320", "7315"],
+          "Hobart": ["7000", "7520"],
+          "Launceston": ["7250", "7334"]
+      },
+      "Victoria": {
+          "Geelong": ["3220", "3221", "3222"],
+          "Melbourne": ["3000", "3001", "3002"]
+      },
+      "Western Australia": {
+          "Broome": ["6725", "6318", "6701"],
+          "Coolgardie": ["6429", "6432"]
+      }
+  },
+  "Brazil": {
+      "Brasilia": ["70000", "70001", "70002"],
+      "Fortaleza": ["60000", "60001", "60002"],
+      "Salvador": ["40000", "40001", "40002"]
+  },
+  "Canada": {
+      "Alberta": {
+          "Calgary": ["8033", "8333", "8035"],
+          "Edmonton": ["9040", "9403", "9040"]
+      },
+      "British Columbia": {
+          "Vancouver": ["V5K 0A1", "V5K 0A2", "V5K 0A3"],
+          "Victoria": ["V8N 1A1", "V8N 1A2", "V8N 1A3"]
+      },
+      "Manitoba": {
+          "Brandon": ["6030", "6030"],
+          "Winnipeg": ["5524", "5547", "5248"]
+      },
+      "Ontario": {
+          "Ottawa": ["K1P 1A1", "K1P 1A2", "K1P 1A3"],
+          "Toronto": ["M5A 1A1", "M5A 1A2", "M5A 1A3"]
+      },
+      "Quebec": {
+          "Montreal": ["H2X 1A1", "H2X 1A2", "H2X 1A3"],
+          "Quebec City": ["G1A 1A1", "G1A 1A2", "G1A 1A3"]
+      },
+      "Saskatchewan": {
+          "Regina": ["S4P 0A1", "S4P 0A2", "S4P 0A3"],
+          "Saskatoon": ["S7H 0A1", "S7H 0A2", "S7H 0A3"]
+      }
+  },
+  "China": {
+      "Guangzhou": ["510000", "510001", "510002"],
+      "Shenzhen": ["518000", "518001", "518002"]
+  },
+  "France": {
+      "Bordeaux": ["33000", "33001", "33002"],
+      "Nice": ["06000", "06001", "06002"],
+      "Toulouse": ["31000", "31001", "31002"]
+  },
+  "Germany": {
+      "Bavaria": {
+          "Munich": ["80331", "80333", "80335"],
+          "Nuremberg": ["90402", "90403", "90404"]
+      },
+      "Hessen": {
+          "Frankfurt": ["60306", "60309", "60310"],
+          "Wiesbaden": ["55246", "55247", "55248", "55249"]
+      },
+      "Lower Saxony": {
+          "Braunschweig": ["38100", "38102", "38104"],
+          "Hanover": ["30159", "30161", "30163"]
+      },
+      "North Rhine-Westphalia": {
+          "Cologne": ["50667", "50668", "50669"],
+          "Dusseldorf": ["40210", "40211", "40212"]
+      },
+      "Saarland": {
+          "Saarbrücken": ["66111", "66113", "66115"],
+          "Völklingen": ["66333", "66346", "66347"]
+      },
+      "Saxony": {
+          "Dresden": ["01067", "01069", "01071"],
+          "Leipzig": ["04103", "04105", "04107"]
+      }
+  },
+  "India": {
+      "Bangalore": ["560001", "560002", "560003"],
+      "Chennai": ["600001", "600002", "600003"],
+      "Kolkata": ["700001", "700002", "700003"]
+  },
+  "Japan": {
+      "Fukuoka": ["810-0001", "810-0002", "810-0003"],
+      "Nagoya": ["460-0001", "460-0002", "460-0003"],
+      "Sapporo": ["060-0001", "060-0002", "060-0003"]
+  },
+  "Mexico": {
+      "Monterrey": ["64000", "64001", "64002"],
+      "Puebla": ["72000", "72001", "72002"]
+  },
+  "Philippines": {
+      "Cebu City": ["6000", "6001", "6002"],
+      "Manila": ["1000", "1001", "1002"],
+      "Quezon City": ["1100", "1101", "1102"]
+  },
+  "Russia": {
+      "Novosibirsk": ["630000", "630001", "630002"],
+      "Yekaterinburg": ["620000", "620001", "620002"]
+  },
+  "South Africa": {
+      "Durban": ["4001", "4002", "4003"],
+      "Pretoria": ["0001", "0002", "0003"]
+  },
+  "United Kingdom": {
+      "England": {
+          "London": ["SW1A 1AA", "SW1A 1AB", "SW1A 1AC"],
+          "Manchester": ["M1 1AA", "M1 1AB", "M1 1AC"]
+      },
+      "Northern Ireland": {
+          "Belfast": ["BT1 1AA", "BT1 1AB", "BT1 1AC"],
+          "Derry": ["BT48 1AA", "BT48 1AB", "BT48 1AC"]
+      },
+      "Scotland": {
+          "Edinburgh": ["EH1 1AA", "EH1 1AB", "EH1 1AC"],
+          "Glasgow": ["G1 1AA", "G1 1AB", "G1 1AC"]
+      },
+      "Wales": {
+          "Cardiff": ["CF1 1AA", "CF1 1AB", "CF1 1AC"],
+          "Swansea": ["SA1 1AA", "SA1 1AB", "SA1 1AC"]
+      }
+  },
+  "United States": {
+      "California": {
+          "Los Angeles": ["90001", "90002", "90003"],
+          "San Francisco": ["94102", "94103", "94104"]
+      },
+      "Florida": {
+          "Miami": ["33101", "33102", "33103"],
+          "Orlando": ["32801", "32802", "32803"]
+      },
+      "Georgia": {
+          "Atlanta": ["30301", "30302", "30303"],
+          "Savannah": ["31401", "31402", "31403"]
+      },
+      "Illinois": {
+          "Chicago": ["60601", "60602", "60603"],
+          "Springfield": ["62701", "62702", "62703"]
+      },
+      "New York": {
+          "Buffalo": ["14201", "14202", "14203"],
+          "New York City": ["10001", "10002", "10003"]
+      },
+      "Texas": {
+          "Austin": ["73301", "73302", "73303"],
+          "Houston": ["77001", "77002", "77003"]
+      }
+  }
+}
+
+
+window.onload = function(){
+  const selectCountry = document.getElementById('country'),
+      selectState = document.getElementById('state'),
+      selectCity = document.getElementById('city'),
+      selectZip = document.getElementById('zip'),
+      selects = document.querySelectorAll('select')
+
+      selectState.disabled = true
+      selectCity.disabled = true
+      selectZip.disabled = true
+
+      selects.forEach(select => {
+          if(select.disabled == true){
+              select.style.cursor = "auto"
+          }
+          else{
+              select.style.cursor = "pointer"
+          }
+      })
+
+      for(let country in countrySateCityinfo){
+          // console.log(country);
+          selectCountry.options[selectCountry.options.length] = new Option(country, country)
+      }
+
+
+      // country change
+      selectCountry.onchange = (e) =>{
+          
+          selectState.disabled = false
+          selectCity.disabled = true
+          selectZip.disabled = true
+
+          selects.forEach(select => {
+              if(select.disabled == true){
+                  select.style.cursor = "auto"
+              }
+              else{
+                  select.style.cursor = "pointer"
+              }
+          })
+
+          selectState.length = 1
+          selectCity.length = 1
+          selectZip.length = 1
+
+          for(let state in countrySateCityinfo[e.target.value]){
+              // console.log(state);
+              selectState.options[selectState.options.length] = new Option(state, state)
+          }
+      }
+
+      // state change
+      selectState.onchange = (e) =>{
+          selectCity.disabled = false
+          selectZip.disabled = true
+
+          selects.forEach(select => {
+              if(select.disabled == true){
+                  select.style.cursor = "auto"
+              }
+              else{
+                  select.style.cursor = "pointer"
+              }
+          })
+
+          selectCity.length = 1
+          selectZip.length = 1
+
+          for(let city in countrySateCityinfo[selectCountry.value][e.target.value]){
+              // console.log(city);
+              selectCity.options[selectCity.options.length] = new Option(city, city)
+          }
+      }
+
+      // change city
+      selectCity.onchange = (e) =>{
+          selectZip.disabled = false
+
+          selects.forEach(select => {
+              if(select.disabled == true){
+                  select.style.cursor = "auto"
+              }
+              else{
+                  select.style.cursor = "pointer"
+              }
+          })
+          
+          selectZip.length = 1
+
+          let zips = countrySateCityinfo[selectCountry.value][selectState.value][e.target.value]
+          
+          for(let i=0; i<zips.length; i++){
+              selectZip.options[selectZip.options.length] = new Option(zips[i], zips[i])
+          }
+      }
+}
